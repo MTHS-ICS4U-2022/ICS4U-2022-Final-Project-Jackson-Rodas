620b82262a3f71f4abb21310e54f9c46a5a3d143
Rodas-Nega1 <90275822+Rodas-Nega1@users.noreply.github.com>
2022-12-02 15:04:38 +0000

    updated

--- js/game.js
+++ js/game.js
@@ -1,10 +1,28 @@
 /* global Phaser */
 
+// Copycright (c) 2022 Rodas Nega All rights reerved
+//
+// Created by: Rodas Nega
+// Created on: Dec 2022
+// This is the Phaser3 configuration file
+
+//* Game Scene */
 const config = {
   type: Phaser.AUTO, 
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autocenter: Phaser.Scale.CENTER_BOTH  
+  }
 }
 
 const game = new Phaser.Game(config)
