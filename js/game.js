b1514e424ea0b33ad09f4de10d50c532fadf2e59
Jackson-Naufal <80114615+JacksonNaufal@users.noreply.github.com>
2022-12-02 14:58:30 +0000

    Fitting the screen

--- js/game.js
+++ js/game.js
@@ -1,10 +1,27 @@
 /* global Phaser */
 
+// Copyright (c) 2022 Jackson Naufal and Rodas Nega All rights reserved
+//
+// Created by: Jackson Naufal and Rodas Nega
+// this is the Phaser3 configuration file
+
+//* Game scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920, 
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: 'arcade',
+    arcade: {
+    debug: true
+    }
+},
+  backgroundColor: 0x5f6e7a,
+  scale: {
+   mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
