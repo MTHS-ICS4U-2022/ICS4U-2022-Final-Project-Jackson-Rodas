f1f3219864a445c37262feb2ecfe1c8e15333fc1
Jackson-Naufal <80114615+JacksonNaufal@users.noreply.github.com>
2023-01-13 14:49:51 -0500

    Update carClass.js

--- js/ThingsThatMove/carClass.js
+++ js/ThingsThatMove/carClass.js
@@ -7,10 +7,10 @@
 // This is the Car class
 
 export default class Car extends Phaser.GameObjects.Sprite {
-  constructor (config) {
+  constructor(config) {
 
     // this supers all the needed information, the scene, the x, the y, the image, and data
-    super(config.scene,config.x,config.y, "car")
+    super(config.scene, config.x, config.y, "car")
 
     // this is a reference we used to find the physics
     // https://www.html5gamedevs.com/topic/37607-add-matter-physics-body-to-gameobjectssprite/
@@ -22,16 +22,26 @@ export default class Car extends Phaser.GameObjects.Sprite {
 
     // Random Speed for each of the cars
     this.speed = Phaser.Math.Between(10, 20)
-    }
+    this.random = Phaser.Math.Between(1, 2)
+  }
 
   update(time, delta) {
 
     // this sets the x at the speed, and makes sure the cars
     // do not go out of bounds, and when they hit the minimum x 
     // coordanite, they reset to the far left of the screen
-    this.x -= this.speed;
-    if (this.x < 60) {
-      this.x = 1920;
+
+    if (this.random === 2) {
+      this.x -= this.speed;
+      if (this.x < 60) {
+        this.x = 1920;
+      }
+    }
+    if (this.random === 1) {
+      this.x += this.speed;
+      if (this.x > 1920) {
+        this.x = 60;
+      }
     }
   }
-}       
+}
