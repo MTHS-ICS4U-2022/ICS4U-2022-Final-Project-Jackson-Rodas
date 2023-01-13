c00971bea64d7e33fc6b40b1b428cc0535861e09
Jackson-Naufal <80114615+JacksonNaufal@users.noreply.github.com>
2023-01-13 14:50:04 -0500

    Update frogClass.js

--- js/ThingsThatMove/frogClass.js
+++ js/ThingsThatMove/frogClass.js
@@ -4,66 +4,44 @@
 //
 // Created by: Jackson Naufal and Rodas Nega
 // Created On: December 2nd 2022
-// This is the Frog class
+// This is the Car class
 
-export default class Frog extends Phaser.GameObjects.Sprite {
-  constructor (config) {
+export default class Car extends Phaser.GameObjects.Sprite {
+  constructor(config) {
 
     // this supers all the needed information, the scene, the x, the y, the image, and data
-    super(config.scene,config.x,config.y, "frog")
-    
+    super(config.scene, config.x, config.y, "car")
+
     // this is a reference we used to find the physics
     // https://www.html5gamedevs.com/topic/37607-add-matter-physics-body-to-gameobjectssprite/
-    // this enables physics for the frog
+    // this enables physics for the car
     config.scene.physics.world.enable(this)
 
     // this adds the image to the scene
     config.scene.add.existing(this)
 
-    // Speed for the frog
-    this.speed = 10
+    // Random Speed for each of the cars
+    this.speed = Phaser.Math.Between(10, 20)
+    this.random = Phaser.Math.Between(1, 2)
   }
-  
-  frogUp() {
 
-    // this adds the maximum movement for the frog going up
-    if (this.y < 50) {
-      this.y = 50
+  update(time, delta) {
+
+    // this sets the x at the speed, and makes sure the cars
+    // do not go out of bounds, and when they hit the minimum x 
+    // coordanite, they reset to the far left of the screen
+
+    if (this.random === 2) {
+      this.x -= this.speed;
+      if (this.x < 60) {
+        this.x = 1920;
+      }
     }
-
-    // this adds the speed in the up direction
-    this.y -= this.speed
-  }
-
-  frogDown() {
-
-    // this adds the minimum movement for the frog going down
-    if (this.y > 1030)
-      this.y = 1030
-
-    // this adds the speed in the down direction
-    this.y += this.speed
-  }
-
-  frogLeft() {
-  
-    // this adds the minimum movement for the frog going left
-    if (this.x < 60) {
-      this.x = 60
+    if (this.random === 1) {
+      this.x += this.speed;
+      if (this.x > 1920) {
+        this.x = 60;
+      }
     }
-  
-    // this adds the speed in the left direction
-    this.x -= this.speed
   }
-
-  frogRight() {
-  
-    // this adds the maximum movement for the frog going right
-    if (this.x > 1860) {
-      this.x = 1860
-    }
-    
-    // this adds the speed in the right direction
-    this.x += this.speed
-  } 
-}       
+}
